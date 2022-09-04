538c2207170795ec17099b8c2d2efbf09ab7fc23
Mosiah Silva <mosiahrabello@gmail.com>
2022-09-04 08:19:38 -0300

    primeiros exerc

---
+++ src/App.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+/*  Curso de Lógica para Programação III
+    Instrutor: Marcio Michelluzzi 
+*/
+public class App {
+    public static void main(String[] args){
+        
+
+        //  Lista de exercícios I
+
+        //  1 - Faça um algoritmo que receba dois números e exiba o resultado da sua soma;
+        //  R:
+        
+        /*  try (Scanner leitorScanner = new Scanner(System.in)) {
+            int a;
+            int b;
+
+            System.out.println("Digite um numero");
+            a = leitorScanner.nextInt();
+            
+            System.out.println("Digite outro numero");
+            b = leitorScanner.nextInt();
+
+            System.out.println("");
+            System.out.println(a + " + " + b + " = " + (a+b));
+            }*/
+        
+    
+    
+            // 2 - Faça um algoritmo que receba dois números e ao final 
+            // mostre a soma, subtração, multiplicação e a divisão dos números lidos;
+            // R:
+            /*
+            try (Scanner sc1 = new Scanner(System.in)) {
+            
+            int a;
+            int b;
+
+            System.out.println("Digite um numero para operacao");
+            a = sc1.nextInt();
+
+            System.out.println("Digite o outro numero da operacao");
+            b = sc1.nextInt();
+
+            System.out.println("Operacao de Soma");
+            System.out.println(a + " + " + b + " = " + (a+b));
+            System.out.println("");
+
+            System.out.println("Operacao de Subtracao");
+            System.out.println(a + " - " + b + " = " + (a-b));
+            System.out.println("");
+
+            System.out.println("Operacao de Multiplicacao");
+            System.out.println(a + " x " + b + " = " + (a*b));
+            System.out.println("");
+
+            System.out.println("Operacao de Divisao");
+            System.out.println(a + " / " + b + " = " + (a/b));
+            System.out.println("");
+
+            }
+            */
+    
+    
+    
+    
+    
+    
+    
+    }
+}
