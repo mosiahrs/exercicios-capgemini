da526ab4f5eb55a9c30c8c473a9524a5ceb99fcf
Mosiah Silva <100864562+mosiahrs@users.noreply.github.com>
2022-09-05 16:38:24 -0300

    Update solved.java
    solved 6-19

--- src/solved.java
+++ src/solved.java
@@ -112,3 +112,286 @@
  * System.out.println("O aluno" + nomeAluno + " possui nota " + mediaAritimetica
  * + " de Media.");
  */
+/*
+ * 6 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * int a;
+ * int b;
+ * int c;
+ * 
+ * System.out.println("Digite o valor de A:");
+ * a = leitor.nextInt();
+ * System.out.println("Digite o valor de B:");
+ * b = leitor.nextInt();
+ * 
+ * c = a;
+ * a = b;
+ * b = c;
+ * 
+ * System.out.println("O valor da variavel A é: " + a);
+ * System.out.println("O valor da variavel B é: " +b);
+ * 
+ */
+/*
+ * 7 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * String temperaturaAtual;
+ * Double C;
+ * Double F;
+ * 
+ * System.out.println("Digite a temperatura em Celsius  (C)");
+ * C = leitor.nextDouble();
+ * 
+ * F = ((9*C)+160) / 5;
+ * 
+ * System.out.println("A temperatura em Fahrenheit é: " + F + "F");
+ */
+/*
+ * 8 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * Double valorReal;
+ * Double cotacaoDolar;
+ * int quantDolar;
+ * 
+ * System.out.println("Digite o valor atual da cotacao do dolar");
+ * cotacaoDolar = leitor.nextDouble();
+ * 
+ * System.out.println("Quantos dólares você possui? Para conversão: ");
+ * quantDolar = leitor.nextInt();
+ * 
+ * valorReal = cotacaoDolar*quantDolar;
+ * 
+ * System.out.println("O valor em real é " + valorReal + " R$");
+ */
+/*
+ * 9 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * Double deposito;
+ * int qtdMeses = 12;
+ * Double juros = 0.0007d;
+ * Double rendimento;
+ * Double saldo = 0.00d;
+ * 
+ * System.out.println("Digite o Valor que deseja depositar: ");
+ * deposito = leitor.nextDouble();
+ * 
+ * saldo = deposito+saldo;
+ * rendimento = deposito+(saldo*juros)*qtdMeses;
+ * 
+ * System.out.println("O valor do rendimento após " + qtdMeses + " mêses é: " +
+ * rendimento + " R$");
+ */
+/*
+ * 10 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * Double valorProduto;
+ * Double valorPrestacao;
+ * int quantidadePrestacao;
+ * Double juros;
+ * 
+ * System.out.println("Digite o valor do Produto");
+ * valorProduto = leitor.nextDouble();
+ * 
+ * System.out.println("Deseja parcelar em quantas vezes :");
+ * quantidadePrestacao = leitor.nextInt();
+ * 
+ * 
+ * if (quantidadePrestacao <= 5){
+ * juros = 0d;
+ * }else{
+ * juros = valorProduto * 0.001d;
+ * }
+ * 
+ * valorPrestacao = (valorProduto/quantidadePrestacao)+juros;
+ * 
+ * System.out.print(
+ * "O produto ficara em " + quantidadePrestacao + " vezes "
+ * + "de ");
+ * System.out.println(String.format("%.2f", valorPrestacao) + " R$");
+ */
+/*
+ * 11 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * Double precoCusto;
+ * Double lucro;
+ * Double precoVenda;
+ * 
+ * System.out.println("Digite o valor de custo do produto: ");
+ * precoCusto = leitor.nextDouble();
+ * 
+ * System.out.println("Qual a porcentagem de lucro (numero decimal)");
+ * lucro = leitor.nextDouble();
+ * 
+ * precoVenda = precoCusto + (precoCusto*lucro);
+ * 
+ * System.out.println("O valor de venda do produto é: " +
+ * precoVenda + " R$");
+ */
+/*
+ * 12 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * Double custoConsumidor;
+ * Double custoFabrica;
+ * Double percDistribuidor;
+ * Double impostos;
+ * 
+ * System.out.println("Digite o custo de fabrica do veículo: ");
+ * custoFabrica = leitor.nextDouble();
+ * 
+ * percDistribuidor = (custoFabrica*28)/100;
+ * System.out.println("distribuidor= " + percDistribuidor);
+ * 
+ * impostos = (custoFabrica+percDistribuidor)*0.45d;
+ * System.out.println("impostos= " + impostos);
+ * 
+ * custoConsumidor = custoFabrica + percDistribuidor + impostos;
+ * System.out.println("O custo para o cliente final é de " +
+ * custoConsumidor + "R$");
+ */
+/*
+ * 13 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * System.out.println("Digite um numero:");
+ * int valor = leitor.nextInt();
+ * 
+ * if (valor > 10){
+ * System.out.println("O valor é maior que 10");
+ * }else if (valor == 10){
+ * System.out.println("O valor é igual a 10");
+ * }else{
+ * System.out.println("O valor é menor que 10");
+ * }
+ */
+/*
+ * 14 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * System.out.println("Digite o valor de A e B: ");
+ * int a = leitor.nextInt();
+ * int b = leitor.nextInt();
+ * 
+ * if (a > b){
+ * System.out.println("O valor de A é maior que B");
+ * }else if ( a == b){
+ * System.out.println("O valor de A é igual a B");
+ * }else{
+ * System.out.println("O valor de B é maior que A");
+ * }
+ */
+/*
+ * 15 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * System.out.println("Digite um numero");
+ * int a = leitor.nextInt();
+ * 
+ * 
+ * if(a >= 100 & a <=200){
+ * System.out.println("O numero está entre 100 e 200");
+ * }else {
+ * System.out.println("O numero não está entre 100 e 200");
+ * }
+ */
+/*
+ * 16 - R:
+ * Scanner leitor = new Scanner(System.in);
+ * 
+ * System.out.println("Digite as notas do semestre: ");
+ * Double n1 = leitor.nextDouble();
+ * Double n2 = leitor.nextDouble();
+ * Double n3 = leitor.nextDouble();
+ * 
+ * Double media = (n1+n2+n3)/3;
+ * 
+ * if (media >=7){
+ * System.out.println(String.format("%.1f", media) + " = Aprovado");
+ * }else if (media > 5.1 & media < 6.9){
+ * System.out.println(String.format("%.1f", media) + " = Recuperação");
+ * }else{
+ * System.out.println(String.format("%.1f", media) + " = Reprovado");
+ * }
+ */
+/*
+ * 17 - R:
+ * import java.util.Random;
+ * Random gerador = new Random();
+ * int count = 0;
+ * 
+ * for (int i = 0; i < 80; i++) {
+ * int n = gerador.nextInt(300);
+ * System.out.println(n);
+ * if (n >= 10 & n <= 150) {
+ * count++;
+ * }
+ * }
+ * System.out.println("");
+ * System.out.println("Foram " + count + " dentro do Intervalo na rodada");
+ */
+/*
+ * 18 - R:
+ * 
+ * Random gerador = new Random();
+ * 
+ * for (int i = 0; i < 75; i++) {
+ * int idade = gerador.nextInt(65);
+ * 
+ * if (idade >= 18) {
+ * System.out.println(idade + " anos" + " - Maior de Idade");
+ * }else {
+ * System.out.println(idade + " anos" + " - Menor de Idade");
+ */
+/*
+ * 19 - R:
+ * 
+ * // Aleatorio - s/nome
+ * 
+ * Random gerador = new Random();
+ * int h = 0;
+ * int m = 0;
+ * 
+ * for (int i = 1; i < 56; i++) {
+ * int n = gerador.nextInt(2);
+ * 
+ * if (n == 0) {
+ * h++;
+ * System.out.println("A pessoa " + i + " é Homem");
+ * } else {
+ * m++;
+ * System.out.println("A pessoa " + i + " é Mulher");
+ * }
+ * 
+ * }
+ * System.out.println("Das 56 pessoas " + h + " são homens e "
+ * + m + " são mulheres!");
+ * 
+ * // Com entradas do Usuário
+ * 
+ * Scanner leitor = new Scanner(System.in);
+ * int h = 0;
+ * int m = 0;
+ * 
+ * for (int i = 0; i < 56; i++) {
+ * System.out.println("Digite seu nome");
+ * String nome = leitor.nextLine();
+ * System.out.println("Digite seu genero (h - homem & m - mulher");
+ * char genero = leitor.nextLine().charAt(0);
+ * 
+ * if (genero == 'h') {
+ * h++;
+ * System.out.println("Olá " + nome + " você é " + " Homem");
+ * } else {
+ * m++;
+ * System.out.println("Olá " + nome + " você é " + " Mulher");
+ * }
+ * }
+ * System.out.println("Total de " + h + " Homens e total de " + m +
+ * " Mulheres");
+ */
