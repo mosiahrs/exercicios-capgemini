76a20b002b3cc1c2eeda9acffbc3345e5e486429
Mosiah Silva <100864562+mosiahrs@users.noreply.github.com>
2022-09-08 14:04:27 -0300

    Update solved.java
    26

--- src/solved.java
+++ src/solved.java
@@ -570,6 +570,39 @@
             }
         }
 */
+/* 26 - R:        
 
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 - 5");
+        int n = leitor.nextInt();
+
+        switch (n) {
+
+            case 1:
+                System.out.println("Um");
+                break;
+
+            case 2:
+                System.out.println("Dois");
+                break;
+
+            case 3:
+                System.out.println("Três");
+                break;
+
+            case 4:
+                System.out.println("Quatro");
+                break;
+
+            case 5:
+                System.out.println("Cinco");
+                break;
+
+            default:
+                System.out.println("número inválido");
+
+        }
+*/
 
 
