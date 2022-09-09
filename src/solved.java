3405580af6208af359f4f207e1945b27667a57e1
Mosiah Silva <100864562+mosiahrs@users.noreply.github.com>
2022-09-09 11:15:03 -0300

    Update solved.java

--- src/solved.java
+++ src/solved.java
@@ -871,7 +871,44 @@
 */
 /* 35 - R: 
 
+        Scanner entrada = new Scanner(System.in);
 
+        float contaDeLuz = 0.0f;
 
+        System.out.println("Informe o consumo total de Kh/h");
+        int consumoEnergia = entrada.nextInt();
 
+        System.out.println("Informe o tipo de cliente");
+        System.out.println("R - Residência");
+        System.out.println("C - Comércio");
+        System.out.println("I - Industria");
+        char tipoCliente = entrada.next().charAt(0);
 
+        if (tipoCliente == 'C' || tipoCliente == 'c') {
+            tipoCliente = 'C';
+        }
+
+        if (tipoCliente == 'R' || tipoCliente == 'r') {
+            tipoCliente = 'R';
+        }
+
+        if (tipoCliente == 'I' || tipoCliente == 'i') {
+            tipoCliente = 'I';
+        }
+
+        switch (tipoCliente) {
+            case 'R' ->
+                contaDeLuz = consumoEnergia * 0.60f;
+            case 'C' ->
+                contaDeLuz = consumoEnergia * 0.48f;
+            case 'I' ->
+                contaDeLuz = consumoEnergia * 1.29f;
+            default ->
+                System.out.println("Tipo inválido");
+        }
+        System.out.println("O valor da conta de Energia é : "
+                + String.format("%.2f", contaDeLuz) + " R$");
+    }
+
+}
+*/
